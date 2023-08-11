07db6aa644d5c198ecc89bcfdd51f8594298280b
kier-ious <kier.diemer@gmail.com>
2023-08-11 14:59:04 -0700

    add n to the error msgs for several funcs

--- pint.c
+++ pint.c
@@ -11,7 +11,7 @@ void pint(stack_t **stack, unsigned int line_number)
 	counter = *stack;
 	if (counter == NULL)
 	{
-		fprintf(stderr, "L%u: can't pint, stack empty", line_number);
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", counter->n);
